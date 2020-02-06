da7369d1203cebb96df72463517948fe76ce82b4
unknown <nafis.github@gmail.com>
2020-02-05 21:45:24 -0500

    Added CSS and increment and decrement

--- src/App.js
+++ src/App.js
@@ -5,23 +5,46 @@ class Counter extends Component{
   constructor(){
     super();
     this.state = {
-      count : 0
+      count : 0,
 
     }
   }
 
 
+// this is where methods live
+increment = () =>{
+  this.setState({
+    count: this.state.count + 1,
+  })
+}
 
-
+decrement = () =>{
+  this.setState({
+    count: this.state.count - 1,
+  })
+}
 
 
   render(){
     return (
 
-      <div>
+      <div className ="container">
 
-      </div>
-    );
+          <div className="navbar">
+            Counter
+          </div>
+
+          <div className="counter">
+            <h1> {this.state.count} </h1>
+
+
+
+            <button type="button" onClick={this.increment}> Increment </button>
+            <button type="button" onClick={this.decrement}> Decrement </button>
+          </div>
+
+        </div>
+    )
   }
 
 }
