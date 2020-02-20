c59958a7f925d83e2fd338613c64353e0ebfb829
unknown <nafis.github@gmail.com>
2020-02-19 20:27:28 -0500

    Need to fix doublecount

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class Counter extends Component{
     super();
     this.state = {
       count : 0,
+      doublecount: false,
 
     }
   }
@@ -13,14 +14,42 @@ class Counter extends Component{
 
 // this is where methods live
 increment = () =>{
-  this.setState({
+if(this.state.count < 20) {
+
+ this.setState({
     count: this.state.count + 1,
   })
 }
+}
 
 decrement = () =>{
+  if(this.state.count > 0) {
   this.setState({
     count: this.state.count - 1,
+  })
+}
+}
+
+
+
+double = () =>{
+  if(this.state.doublecount){
+    this.setState({
+      doublecount: false,
+      count: this.state.count + 2,
+    })
+
+  }
+  else{
+    this.setState({
+      doublecount: true,
+    })
+  }
+}
+
+clear = () =>{
+  this.setState({
+    count: 0,
   })
 }
 
@@ -41,6 +70,8 @@ decrement = () =>{
 
             <button type="button" onClick={this.increment}> Increment </button>
             <button type="button" onClick={this.decrement}> Decrement </button>
+            <button type="button" onClick={this.clear}> Clear </button>
+            <button type="button" onClick={this.double}> {this.state.doublecount} ? "Double Count" : "Single Count" </button>
           </div>
 
         </div>
